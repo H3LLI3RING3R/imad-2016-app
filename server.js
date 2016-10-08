051f2c7d55ddeec9bffaa7a4fef5cf910e32bf2a
H3LLI3RING3R <terminator0211@gmail.com>
2016-10-08 20:24:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,12 +11,27 @@ app.get('/', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+})
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('a1', function (req, res) {
+  res.sendFile('It will be served');
+})
+app.get('a4', function (req, res) {
+  res.sendFile('It will be served');
+})
+
+app.get('a5', function (req, res) {
+  res.sendFile('It will be served');
+})
+
+
+
+
+
+
+app.get('/u;i/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
