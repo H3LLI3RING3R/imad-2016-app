4818f68f312165873e2c51665f5aec0eeb1923ec
H3LLI3RING3R <terminator0211@gmail.com>
2016-10-08 20:27:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 })
 
-app.get('a1', function (req, res) {
+app.get('/a1', function (req, res) {
   res.sendFile('It will be served');
 })
-app.get('a4', function (req, res) {
+app.get('/a4', function (req, res) {
   res.sendFile('It will be served');
 })
 
-app.get('a5', function (req, res) {
+app.get('/a5', function (req, res) {
   res.sendFile('It will be served');
 })
 
