f0c0072933faa69b1da0f5da162c81fafc90430d
H3LLI3RING3R <terminator0211@gmail.com>
2016-10-08 20:50:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 app.get('/a1', function (req, res) {
-  res.send('It will be served');
+    res.sendFile(path.join(__dirname, 'ui', 'a1.html'));
+ 
 })
 
 app.get('/a2', function (req, res) {
