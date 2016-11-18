da9f6180975a48b8528eee61d1707423d6d4234b
H3LLI3RING3R <terminator0211@gmail.com>
2016-11-18 14:49:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-console.log('Loaded!');
+// Counter button coding
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function()
+{
+    
+    counter++;
+  var span = deocument.getElementById('count');
+  spaninnerHTML = counter.toString();
+    
+};
