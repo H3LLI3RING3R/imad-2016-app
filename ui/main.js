be3f78f67617e3e1a0d0542ee49b01cd6a16a602
H3LLI3RING3R <terminator0211@gmail.com>
2016-11-18 15:04:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,26 @@ var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function()
 
-{ counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+{   //Creating a request
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        
+        {  if(request.status === 200)
+        {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+       }
+            }
+    };
+    
+    
+    // Making a request
+    request.open('GET','http://h3lli3ring3r.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
     
 };
