7c11ec0392095c71e58a98cb6a7c7479766e52c6
H3LLI3RING3R <terminator0211@gmail.com>
2016-11-18 10:36:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 app.get('/a1', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'krk.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
  
 });
 
